43eac943965ac8822f7b7c98f3215c695ed48f30
El Reyziku Adipura <virmanadipuraaa@gmail.com>
2024-08-09 07:03:50 +0700

    Mengubah file script.js index.html dan style.css

--- script.js
+++ script.js
@@ -1,24 +1,15 @@
+const nameList = [];
 
-const nmBrg = ["Roti Aoka", "Roti Padimas", "Roti Buaya"];
+document.querySelector('.input-name').onsubmit = function (event) {
+    
+    event.preventDefault();
 
-// Basic Method Array
+    const name = document.forms['nameForm']['name'].value;
 
-console.info(`Isi Box : ${nmBrg}`) // Syntax untuk mengetahui isi value yang ada didalam Array
-console.info(`Jumlah Barang : ${nmBrg.length}`); // Syntax untuk Panjang atau Jumlah Array
-console.info(`Barang ke -1 : ${nmBrg[0]}`); // Syntax untuk mengetahui value dengan metode memanggil index
-console.info(`Barang ke -2 : ${nmBrg[1]}`); // Barang ke -2
-console.info(`Barang ke -3 : ${nmBrg[2]}`); // Barang ke -3
+    nameList.push(name);
 
-console.info("--------------------------------") // Garis Pembatas
+    document.forms['nameForm'].reset();
 
-// For Each
-
-nmBrg.forEach((value, index) => console.info(`Barang ke -${index + 1} : ${value}`));
-
-console.info("--------------------------------") // Garis Pembatas
-
-// For i
-
-for (let i = 0; i < nmBrg.length; i++) {
-  console.info(`Isi Box ${i + 1} : ${nmBrg[i]}`);
-}
+    console.info(nameList);
+  
+};
